c95a254f1fb89cb80383430d12c64124c12e235f
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-31 00:33:48 -0500

    FUNCTIONS CREATED : create() + display()

--- LinkedLists/LinkedList_MiniProject.c
+++ LinkedLists/LinkedList_MiniProject.c
@@ -15,16 +15,110 @@ void search();
 
 struct node {
     int data;
-    struct Node* next;
+    struct node* next;
 };
 
 //declaring a global variable
-struct Node *head_pointer = NULL;
+struct node *head_pointer = NULL;
 
 int main(){
     
     int ch;
 
+    while(1) {
+        printf("\n*** S E L E C T  F R O M  T H E  F O L L O W I N G ***");
+        printf("\n1.  CREATE");
+        printf("\n2.  DISPLAY");
+        printf("\n3.  INSERT Node at the start of the list");
+        printf("\n4.  INSERT Node at the end of the list");
+        printf("\n5.  INSERT Node at a specific position in the list");
+        printf("\n6.  SEARCH data of a Node in the list");
+        printf("\n7.  SORT the given linked list");
+        printf("\n8.  REVERSE the given linked list");
+        printf("\n9.  DELETE Node at the start of the list");
+        printf("\n10. DELETE Node at the end of the list");
+        printf("\n11. DELETE Node at a specific position in the list");
+        printf("\n12. EXIT or QUIT");
+        printf("\n******************************************************");
 
+        printf("\n\nENTER your choice :  ");
+        scanf("%d",&ch);
+    
+        switch (ch)
+        {
+        case 1 : 
+            create();
+            break;
+        case 2 : 
+            delete();
+            break;
+        case 3 : 
+            insert_at_start();
+            break;
+        case 4 : 
+            insert_at_end();
+            break;
+        case 5 : 
+            insert_at_pos();
+            break;
+        case 6 : 
+            search();
+            break;
+        case 7 : 
+            sort();
+            break;
+        case 8 : 
+            reverse();
+            break;
+        case 9 : 
+            delete_at_start();
+            break;
+        case 10 : 
+            delete_at_end();
+            break;
+        case 11 : 
+            delete_at_pos();
+            break;
+        case 12 : 
+            exit(0);
+            break;
+
+        default:
+        printf("\nInvalid Choice entered");
+            break;
+        }
+    }
     return 0;
+}
+
+void create(){
+    struct node *p, *s1;
+    p = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter data :  ");
+    scanf("%d",&p->data);
+    p->next=NULL;
+
+    if( head_pointer ==NULL)
+        head_pointer = p;
+    else{
+        s1 = head_pointer;
+        while( s1->next != NULL)
+            s1 = s1->next;
+        s1->next = p;
+    }
+}
+
+void display(){
+    struct node * s1;
+    if(head_pointer == NULL){
+        printf("\nLinked list is EMPTY...");
+        return;
+    }
+    else{
+        s1 = head_pointer;
+        printf("\nELEMENTS in the linked list are : \n");
+        while(s1 != NULL){
+            printf("%d\t",s1->data);
+        }
+    }
 }
