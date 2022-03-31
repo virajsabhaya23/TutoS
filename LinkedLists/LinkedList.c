f248e066328784b9130bea0e38a2af69806dcf56
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-30 23:30:53 -0500

    node + variables assigned values

---
+++ LinkedLists/LinkedList.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node {
+    //variable to store the value of the data in a particular index of the linked list
+    int data;
+    struct Node* next;
+};
+
+
+int main(){
+
+
+    return 0;
+}
