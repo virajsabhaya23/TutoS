9a3771bd7b8c70382d944612dfb7f6d64e7c2128
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-30 23:33:43 -0500

    10 methods created

--- LinkedLists/LinkedList.c
+++ LinkedLists/LinkedList.c
@@ -1,14 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void create();
+void display();
+void insert_at_start();
+void insert_at_end();
+void insert_at_pos();
+void delete_at_start();
+void delete_at_end();
+void delete_at_pos();
+void reverse();
+void sort();
+void search();
+
 struct node {
-    //variable to store the value of the data in a particular index of the linked list
     int data;
     struct Node* next;
 };
 
+//declaring a global variable
+struct Node *head_pointer = NULL;
 
 int main(){
+    
+    int ch;
 
 
     return 0;
